dc7760862a0c6c6365a47bad3c96eeda117acca3
dwinuryanto <yantodwinur29@gmail.com>
2020-07-06 17:33:34 +0700

    Tugas Responsi Pemograman (19.11.2624)

---
+++ Produk.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectProduk
+{
+    public class Produk
+    {
+        // PERINTAH: lengkapi property class produk, sesuai petunjuk soal
+        public string KodeProd { get; set; }
+        public string NamaProd { get; set; }
+        public double HarBeli { get; set; }
+        public double HarJual { get; set; }
+    }
+}
